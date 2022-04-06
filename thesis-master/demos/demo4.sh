65d804f280c74e0a046bfd6d5a299b88ddd4bd31
Spencer Imbleau <spencer@imbleau.com>
2022-04-06 17:22:33 -0400

    Finalized presentation notes and demos

--- thesis-master/demos/demo4.sh
+++ thesis-master/demos/demo4.sh
@@ -3,4 +3,5 @@
 BIN="/home/simbleau/git/simbleau/thesis-master/demos/bin/warp-sandbox"
 ROOT_FOLDER="/home/simbleau/git/simbleau/thesis-master/demos/"
 
+cd $ROOT_FOLDER
 exec env CARGO_MANIFEST_DIR=$ROOT_FOLDER $BIN
